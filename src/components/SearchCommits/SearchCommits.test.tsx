33b79fc9658e3dbc02a4d0cb1550e8cacd6e2319
Skye Anico <skye.anico@gmail.com>
2022-10-14 16:38:44 -0400

    created initial tests for searchcommit component

---
+++ src/components/SearchCommits/SearchCommits.test.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { render, screen, waitFor, cleanup } from '@testing-library/react';
+
+describe('Search Commits', () => {
+	test('Search button should be disabled on load', () => {});
+});
