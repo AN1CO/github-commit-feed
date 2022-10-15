1d289bb5006f32b286b9710bef2d725a04fdcb10
Skye Anico <skye.anico@gmail.com>
2022-10-15 15:32:44 -0400

    created tests for seach commits. still need to make mock data for dispaly commits

--- src/components/SearchCommits/SearchCommits.test.tsx
+++ src/components/SearchCommits/SearchCommits.test.tsx
@@ -1,6 +1,38 @@
-import React from 'react';
-import { render, screen, waitFor, cleanup } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import SearchCommits from './SearchCommits';
 
 describe('Search Commits', () => {
-	test('Search button should be disabled on load', () => {});
+	test('Submit button should be disabled when there is no text in input boxes', () => {
+		render(<SearchCommits />);
+		const userSearchElement = screen.getByPlaceholderText('Search User');
+		const repoSearchElement = screen.getByPlaceholderText('Search Repo');
+		const buttonElement = screen
+			.getAllByRole('button', { name: 'Submit' })
+			.pop();
+		fireEvent.change(userSearchElement, { target: { value: '' } });
+		fireEvent.change(repoSearchElement, { target: { value: '' } });
+		expect(buttonElement).toBeDisabled();
+	});
+	test('Search button should be disabled when there is text in only one input box', () => {
+		render(<SearchCommits />);
+		const userSearchElement = screen.getByPlaceholderText('Search User');
+		const repoSearchElement = screen.getByPlaceholderText('Search Repo');
+		const buttonElement = screen
+			.getAllByRole('button', { name: 'Submit' })
+			.pop();
+		fireEvent.change(userSearchElement, { target: { value: 'user' } });
+		fireEvent.change(repoSearchElement, { target: { value: '' } });
+		expect(buttonElement).toBeDisabled();
+	});
+	test('Submit button should enable when text is written in both input boxes', () => {
+		render(<SearchCommits />);
+		const userSearchElement = screen.getByPlaceholderText('Search User');
+		const repoSearchElement = screen.getByPlaceholderText('Search Repo');
+		const buttonElement = screen
+			.getAllByRole('button', { name: 'Submit' })
+			.pop();
+		fireEvent.change(userSearchElement, { target: { value: 'user' } });
+		fireEvent.change(repoSearchElement, { target: { value: 'repo' } });
+		expect(buttonElement).not.toBeDisabled();
+	});
 });
